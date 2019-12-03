b9df8cf8a877c923a6090f3baa08a3e022178a21
Mikan0123 <yunyon.10x8mf.wcrb@gmail.com>
2019-12-03 23:33:37 +0900

    add split string

--- mypractice/src/mypractice/Print.java
+++ mypractice/src/mypractice/Print.java
@@ -1,5 +1,7 @@
 package mypractice;
 
+import java.util.StringTokenizer;
+
 public class Print {
 	public static void main(String[] args) {
 		int a = 10;
@@ -9,13 +11,22 @@ public class Print {
 		int d = c.length();
 		String e = c.substring(1,c.length());
 		int f = e.compareTo(c1);
+		String g = "I,love,Dog";
 
 		System.out.println("a = " + a);
 		System.out.println("b = " + b);
 		System.out.println("c = " + c);
 		System.out.println("c1 = " + c1);
 		System.out.println("d = " + d);
-		System.out.println("b = " + e);
-		System.out.println("b = " + f);
+		System.out.println("e = " + e);
+		System.out.println("f = " + f);
+		System.out.println("g = " + g);
+
+		StringTokenizer h = new StringTokenizer(g,",");
+		String k = h.nextToken();
+		while(h.hasMoreTokens()) {
+			k = k + " " + (h.nextToken());
+		}
+		System.out.println("k = " + k);
 	}
 }
